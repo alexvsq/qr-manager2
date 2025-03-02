0f1d74bf99326c9764c7c4e9d11dc0b232556beb
user <alejandro79861@gmail.com>
2025-03-02 16:14:26 -0500

    agregar color azul

--- src/app/(tabs)/settings.tsx
+++ src/app/(tabs)/settings.tsx
@@ -146,7 +146,7 @@ const styles = StyleSheet.create({
         ...SHADOW_DEFAULT
     },
     button2: {
-        backgroundColor: COLORS.blackBg,
+        backgroundColor: COLORS.blue,
         paddingVertical: 6,
         paddingHorizontal: 12,
         borderRadius: 10,
